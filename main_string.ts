ee3c1002318009d02c20a55d5b210f9b13e1ae5f
irynajevaga <163996627+irynajevaga@users.noreply.github.com>
2024-10-27 01:14:07 +0200

    Initial commit

---
+++ main_string.ts
@@ -0,0 +1,10 @@
+
+import { capitalize, reverseString } from './stringUtils'; // Импорт функций
+
+// Тестирование функции capitalize
+const testString1 = "hello";
+console.log(capitalize(testString1)); // Вывод: "Hello"
+
+// Тестирование функции reverseString
+const testString2 = "world";
+console.log(reverseString(testString2)); // Вывод: "dlrow"
